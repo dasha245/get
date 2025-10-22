d8c458787e4adec8e4dcdd6e2fa0cebe473471a5
dasha245 <frolovadarya2007@gmail.com>
2025-10-22 18:30:58 +0300

    роврывп

--- get-adc/r2r_adc.py
+++ get-adc/r2r_adc.py
@@ -20,16 +20,17 @@ class R2R_ADC:
 
     def number_to_dac(self, number):
             a = [int(element) for element in bin(number)[2:].zfill(8)]
-            GPIO.output(self.gpio_bits, a)
+            for i, pin in enumerate(self.gpio_bits):
+                GPIO.output(pin, a[i])
 
     def sequential_counting_adc(self):
-        for number in range (0, 256):
+        for number in range (256):
             self.number_to_dac(number)
             time.sleep(self.compare_time)
-            if GPIO.input(self.comp_gpio):
+            if GPIO.input(self.comp_gpio) == 1:
                 return number
 
-            return 255
+        return 255
 
     def get_sc_voltage(self):
         return (self.sequential_counting_adc()/255)*self.dynamic_range
